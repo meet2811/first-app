f031ddcb534fbcc9093b31373dfd1e010974e96f
meet2811 <75978190+meet2811@users.noreply.github.com>
2025-06-20 20:24:41 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.write("hello world")
+x = st.text_input("movie", "star wars")
+
+if st.button("click"):
+    st.write(f"your favorite movie is `{x}`")
+
+data = pd.read_csv("movies.csv")
+st.write(data)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["A", "B", "C"])
+st.bar_chart(chart_data)
